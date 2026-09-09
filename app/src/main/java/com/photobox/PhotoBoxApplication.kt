0b56cb1d24f69e4e937300f2986bec45114e7ec2
PhotoBox Dev <dev@photobox.local>
2026-09-09 15:29:59 +0800

    feat(applock): register AppLockLifecycleObserver in Application

--- app/src/main/java/com/photobox/PhotoBoxApplication.kt
+++ app/src/main/java/com/photobox/PhotoBoxApplication.kt
@@ -1,7 +1,18 @@
 package com.photobox
 
 import android.app.Application
+import androidx.lifecycle.ProcessLifecycleOwner
+import com.photobox.feature.applock.AppLockLifecycleObserver
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class PhotoBoxApplication : Application()
+class PhotoBoxApplication : Application() {
+
+    @Inject lateinit var appLockLifecycleObserver: AppLockLifecycleObserver
+
+    override fun onCreate() {
+        super.onCreate()
+        ProcessLifecycleOwner.get().lifecycle.addObserver(appLockLifecycleObserver)
+    }
+}
